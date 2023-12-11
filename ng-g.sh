0aa4acd2f4ff35846447153c32a31aec277f8443
Dom Hankle <domhankle03@gmail.com>
2023-12-11 15:01:03 -0500

    Exiting on no first command

--- ng-g.sh
+++ ng-g.sh
@@ -8,12 +8,12 @@ DIRECTORY=${SING_DIR}/src/app/$2
 checkArgs(){
   if [ -z $COMPONENT_NAME ]; then
     echo You need to specify a component name;
-    return 1  
+    exit 1  
   fi
 
   if [ -z $DIRECTORY ]; then
     echo You need to specify a directory path for the component;
-    return 1
+    exit 1
   fi
   return 0
 
@@ -22,7 +22,7 @@ checkArgs(){
 checkEnv(){
   if [ -z $DEV_DIR ]; then
     echo You need to set the DEV_DIR environmental variable in your bashrc.
-    return 0
+    exit 1
   fi
 }
 
